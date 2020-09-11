7fc1ebabbe466d3371036c31e3e2a32559fa76ce
LL <ll@itcast.cn>
2020-09-11 18:10:28 +0800

    完成个人中心模块

---
+++ assets/js/user.avatar.js
@@ -0,0 +1,52 @@
+$(function () {
+    // 1.1 获取裁剪区域的 DOM 元素
+    var $image = $('#image')
+    // 1.2 配置选项
+    const options = {
+        // 纵横比
+        aspectRatio: 1,
+        // 指定预览区域
+        preview: '.img-preview'
+    }
+
+    // 1.3 创建裁剪区域
+    $image.cropper(options)
+
+    //为上传绑定点击事件
+    $('#btn_img').on('click', function () {
+        $('#file').click()
+    })
+    var layer = layui.layer
+    //监听file的change事件
+    $('#file').on('change', function (e) {
+        var filelist = e.target.files
+        if (filelist.length == 0) {
+            return layer.msg('1213')
+        }
+        var file = filelist[0]
+        var newImgURL = URL.createObjectURL(file)
+        $image
+            .cropper('destroy')
+            .attr('src', newImgURL)
+            .cropper(options)
+        var dataURL = $image
+            .cropper('getCroppedCanvas', {
+                width: 100,
+                height: 100
+            })
+            .toDataURL('image/png')
+        $.ajax({
+            method: 'POST',
+            url: '/my/update/avatar',
+            data: {
+                avatar: dataURL
+            },
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('失败')
+                }
+                window.parent.getUserInfo()
+            }
+        })
+    })
+})
---
+++ assets/js/user_info.js
@@ -0,0 +1,48 @@
+$(function () {
+    //验证表单
+    var form = layui.form
+    form.verify({
+        nickname: function (value, item) {
+            if (value.length > 6) {
+                return '昵称长度应为6~12位的字符';
+            }
+        }
+    });
+    //初始化用户的基本信息
+    var layer = layui.layer
+    initUserInfo()
+    var userinfo = null
+    function initUserInfo() {
+        $.ajax({
+            type: 'GEST',
+            url: '/my/userinfo',
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+                form.val(formUserInfo, res.data);
+                userinfo = res.data
+            }
+        })
+    }
+    //设置重置事件
+    $('#btnReset').on('click', function (e) {
+        e.preventDefault()
+        // initUserInfo()
+        form.val(formUserInfo, userinfo);
+    })
+    $('.layui-form').on('submit', function (e) {
+        e.preventDefault()
+        $.ajax({
+            type: "POST",
+            url: '/my/userinfo',
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('更新用户信息失败')
+                }
+                layer.msg('更新用户信息成功')
+                window.parent.initUserInfo()
+            }
+        })
+    })
+})
