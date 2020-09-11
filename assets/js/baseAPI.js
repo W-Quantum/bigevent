fb3428ea562832693731e0c288e0f559c32f1aed
LL <ll@itcast.cn>
2020-09-11 11:29:10 +0800

    index页面完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,17 @@
 $.ajaxPrefilter(function (options) {
+    //统一设置路径
     options.url = 'http://ajax.frontend.itheima.net' + options.url
-
+    // 统一设置有权限的请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    //全局统一挂载 complete 回调函数
+    options.complete = function (res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败!') {
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,52 @@
+$(function () {
+    getUserInfo()
+    var layer = layui.layer
+    //弹出框
+    $('#btn_ogout').on('click', function () {
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+            //关闭询问弹出框
+            layer.close(index);
+        });
+    })
+})
+
+//获取用户信息
+function getUserInfo() {
+    $.ajax({
+        type: 'GET',
+        url: '/my/userinfo',
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function (res) {
+            if (res.status !== 0) {
+                return layer.msg('获取失败');
+            }
+            renderAvatar(res.data)
+        }
+        // complete: function (res) {
+        //     if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败') {
+        //         localStorage.removeItem('token')
+        //         location.href = '/login.html'
+        //     }
+        // }
+    })
+}
+//渲染用户的头像
+function renderAvatar(user) {
+    var name = user.nickname || user.username
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    //
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avatar'), html(first).show()
+    }
+
+}
